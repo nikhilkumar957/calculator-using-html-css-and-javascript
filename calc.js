b3e1f9cc6f500890311df51f19ae393f5fb706aa
nikhilkumar957 <101115421+nikhilkumar957@users.noreply.github.com>
2022-03-09 18:50:32 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,14 @@
+window.alert("enter the operator")
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+   }
+   function display(value) {
+    document.getElementById("result").value += value;
+   }
+  
+   function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
